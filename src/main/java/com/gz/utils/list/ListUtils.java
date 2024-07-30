ccab5c56961ec59085d9ea0310d6a107539a0226
gao.zhen <1586049233@qq.com>
2024-07-30 15:21:15 +0800

    添加山东省地市列表获取方法及代码格式优化
    本次代码更新包括：
    - 新增了一个方法`getSDList`，用于获取山东省各地市的名称列表。该方法将一个包含所有地市名称的字符串按逗号分割后，转换为列表返回。
    - 对代码格式进行了微调，例如修改了部分注释的格式，以提高代码的可读性和维护性。

--- src/main/java/com/gz/utils/list/ListUtils.java
+++ src/main/java/com/gz/utils/list/ListUtils.java
@@ -1,6 +1,7 @@
 package com.gz.utils.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -17,8 +18,8 @@ public class ListUtils {
      * 此方法主要用于将一个大的列表分解成小的、更易处理的列表。
      *
      * @param visitList 原始的访问列表，包含需要分割的字符串元素。
-     * @param num 指定的分割数，用于确定每个子列表的大致长度。
-     * @return 返回一个包含多个子列表的列表。每个子列表都尽量包含相同数量的元素。
+     * @param num 指定的分割数，用于确定每个子列表的大致长度。eg:3
+     * @return 返回一个包含多个子列表的列表。每个子列表都尽量包含相同数量的元素。(不是整除最后一个数量不够)
      */
     private static List<List<String>> getLists(ArrayList<String> visitList, int num) {
         return IntStream.range(0, visitList.size())
@@ -28,6 +29,21 @@ public class ListUtils {
                 .stream()
                 .map(indices -> indices.stream().map(visitList::get).collect(Collectors.toList()))
                 .collect(Collectors.toList());
+    }
+
+   /**
+    * 获取山东省各地市的列表。
+    *
+    * 该方法通过将字符串按照逗号分隔，转换为字符串数组，进而封装为列表形式返回。
+    * 这样做的目的是为了方便对山东省各地市的名称进行集合管理，例如遍历展示或进一步处理。
+    *
+    * @return 返回一个包含山东省各地市名称的字符串列表。
+    */
+   private static List<String> getSDList() {
+        // 定义字符串包含山东省所有地市的名称，各名称之间用逗号分隔。
+        String mapStr = "济南,青岛,淄博,枣庄,东营,烟台,潍坊,威海,济宁,德州,聊城,滨州,菏泽,泰安,临沂,日照";
+        String[] split = mapStr.split(",");
+        return new ArrayList<>(Arrays.asList(split));
     }
 
 }
