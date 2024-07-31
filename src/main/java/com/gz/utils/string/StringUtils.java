ad07218189175203623dfc096ac80c979f7bc76a
gao.zhen <1586049233@qq.com>
2024-07-31 09:36:47 +0800

    新增StringUtils类的concatParams方法在StringUtils类中添加了一个新方法concatParams，该方法用于将多个对象合并为一个字符串，使用指定的分隔符分隔每个对象。此方法支持可变参数，可以接收任意数量的参数。如果未提供参数，方法将返回null。

--- src/main/java/com/gz/utils/string/StringUtils.java
+++ src/main/java/com/gz/utils/string/StringUtils.java
@@ -27,4 +27,27 @@ public class StringUtils {
 
         return sb.toString();
     }
+    /**
+     * 合并参数为一个字符串。
+     * 使用指定的分隔符将多个对象连接成一个字符串。此方法允许接收任意数量的参数。
+     * 如果没有提供参数，则返回null。
+     *
+     * @param spliterStr 分隔符，用于分隔每个对象。注意，分隔符本身不会被添加到字符串的开始或结束。
+     * @param objs 可变参数，要连接的对象。可以是任意类型的对象，它们将被转换为字符串形式。
+     * @return 返回一个字符串，包含所有参数按分隔符连接的结果。如果参数objs为null，则返回null。
+     */
+    public static String concatParams(String spliterStr, Object... objs) {
+
+        StringBuilder sb = new StringBuilder();
+        if (objs != null) {
+            for (int i = 0; i < objs.length; i++) {
+                sb.append(objs[i]);
+                if (i != objs.length - 1) {
+                    sb.append(spliterStr);
+                }
+            }
+            return sb.toString();
+        }
+        return null;
+    }
 }
